7f4ef5ce5620f93c1627ba899568566b1830f48d
Braden Bledsoe <batfat00@byu.edu>
2024-03-21 00:40:15 -0600

    added unit tests for login, register

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -3,10 +3,14 @@ package clientTests;
 import dataAccess.*;
 import exception.ResponseException;
 import model.Auth;
+import model.User;
 import org.junit.jupiter.api.*;
 import server.Server;
 import server.ServerFacade;
+import server.requests.CreateGameRequest;
 import server.requests.LoginRequest;
+import server.requests.RegisterRequest;
+import server.responses.CreateGameSuccessResponse;
 import service.AuthService;
 
 
@@ -14,19 +18,28 @@ public class ServerFacadeTests {
 
     private static Server server;
     static ServerFacade facade;
+    private static AuthDAO authDAO;
+    private static GameDAO gameDAO;
+    private static UserDAO userDAO;
+
 
     @BeforeAll
-    public static void init() {
+    public static void init() throws DataAccessException {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade(String.format("http://localhost:%d", port));
+        authDAO = new SQLAuthDAO();
+        gameDAO = new SQLGameDAO();
+        userDAO = new SQLUserDAO();
+
     }
 
     @BeforeEach
     public void clear() throws DataAccessException {
-        AuthService service = new AuthService(new MemoryAuthDAO(), new MemoryGameDAO(), new MemoryUserDAO());
-        service.clearApp();
+        authDAO.clearAuths();
+        gameDAO.clearGames();
+        userDAO.clearUsers();
     }
 
     @AfterAll
@@ -35,21 +48,66 @@ public class ServerFacadeTests {
     }
 
     @Test
-    void positiveTestlogin() throws ResponseException {
-        LoginRequest login = new LoginRequest("billy", "passwordforBilly")
+    void positiveRegisterTest() throws ResponseException {
+        User req = new User("juan", "something", "random@email");
+        Auth auth = facade.register(req);
+        String actual = auth.username();
+        String expected = "juan";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void negativeRegisterTest() throws ResponseException {
+        User req = new User("juan", "something", "random@email");
+        facade.register(req);
+
+        Assertions.assertThrows(exception.ResponseException.class, () -> {
+            facade.register(req);
+        });
+    }
+
+    @Test
+    void positiveLoginTest() throws ResponseException {
+        User req = new User("juan", "something", "random@email");
+        facade.register(req);
+        LoginRequest login = new LoginRequest("juan", "something");
         Auth auth = facade.login(login);
         String actual = auth.username();
-        String expected = "billy";
+        String expected = "juan";
 
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void register() {
+    void negativeLoginTest() throws ResponseException {
+        LoginRequest login = new LoginRequest("juan", "something");
+
+        Assertions.assertThrows(exception.ResponseException.class, () -> {
+            facade.login(login);
+        });
     }
 
     @Test
-    void createGame() {
+    void positiveCreateGameTest() throws ResponseException {
+        User user = new User("juan", "something", "random@email");
+        facade.register(user);
+        CreateGameRequest req = new CreateGameRequest();
+        req.setGameName("testGame");
+        CreateGameSuccessResponse actual = facade.createGame(req);
+
+        Assertions.assertNotNull(actual);
     }
+    @Test
+    void negativeCreateGameTest() throws ResponseException {
+        CreateGameRequest req = new CreateGameRequest();
+        req.setGameName("testGame");
+
+        Assertions.assertThrows(exception.ResponseException.class, () -> {
+            facade.createGame(req);
+        });
+    }
+
 
     @Test
     void joinGame() {
